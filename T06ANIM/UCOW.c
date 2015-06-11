8ba84614d5dc5858fe1c5226de4e7243c19578d9
spr07 <lisenok5701@yandex.ru>
2015-06-11 19:06:01 +0400

    New reliaze

--- T06ANIM/UCOW.c
+++ T06ANIM/UCOW.c
@@ -18,7 +18,8 @@ typedef struct tagao5UNIT_MODEL
 {
   AO5_UNIT_BASE_FIELDS;
   ao5GOBJ Model;
-
+  INT X, Y, Z;
+  INT RX, RY, RZ;
 } ao5UNIT_MODEL;
 
 /* Функция инициализации объекта анимации.
@@ -68,34 +69,28 @@ static VOID AO5_AnimUnitClose( ao5UNIT_MODEL *Uni, ao5ANIM *Ani )
  */
 static VOID AO5_AnimUnitResponse( ao5UNIT_MODEL *Uni, ao5ANIM *Ani )
 {
-  /*INT i;
+  
   if(Ani->JPOV == 1 || Ani->Keys['W'] == 1)
-    Uni->Y -= 10;
+    Uni->Y += 1;
   if(Ani->JPOV == 3 || Ani->Keys['D'] == 1)
-    Uni->X += 10;
+    Uni->X += 1;
   if(Ani->JPOV == 5 || Ani->Keys['S'] == 1)
-    Uni->Y += 10;
+    Uni->Y -= 1;
   if(Ani->JPOV == 7 || Ani->Keys['A'] == 1)
-    Uni->X -= 10;*/
-  /*
+    Uni->X -= 1;
+  
   if(Ani->JZ == 1 || Ani->Keys['1'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateX(Uni->ObjV[i], -1);
+      Uni->RX = -1;
   if(Ani->JZ == -1 || Ani->Keys['2'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateX(Uni->ObjV[i], 1);
+      Uni->RX = 1;
   if(Ani->JX == 1 || Ani->Keys['3'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateY(Uni->ObjV[i], 1);
+      Uni->RY = 1;
   if(Ani->JX == -1 || Ani->Keys['4'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateY(Uni->ObjV[i], -1);
+      Uni->RY = -1;
   if(Ani->JY == 1 || Ani->Keys['5'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateZ(Uni->ObjV[i], -1);
-  if(Ani->JY == -1 || Ani->Keys['6'])
-    for(i = 0; i < Uni->ObjNumOfV; i++)
-      Uni->ObjV[i] = RotateZ(Uni->ObjV[i], 1);  */
+      Uni->RZ = -1;
+  if(Ani->JY == -1 || Ani->Keys['6']) 
+     Uni->RZ = 1;
   
 } /* End of 'AO5_AnimUnitResponse' function */
 
@@ -111,13 +106,15 @@ static VOID AO5_AnimUnitRender( ao5UNIT_MODEL *Uni, ao5ANIM *Ani )
 {
   SetDCBrushColor(Ani->hDC, RGB(255, 255, 255));
 
-  AO5_RndMatrView = MatrView(VecSet(30, 30, 30),
+  AO5_RndMatrView = MatrView(VecSet(0, 0, 100),
                              VecSet(0, 0, 0),
                              VecSet(0, 1, 0));
-  AO5_RndMatrWorld = 
+  /*AO5_RndMatrWorld = 
     MatrMulMatr(MatrMulMatr(MatrMulMatr(MatrTranslate(Ani->JX * 59, Ani->JY * 88, 0), MatrScale(1, 1, 1)),
     MatrRotateY(30 * Ani->Time + Ani->JR * 180)),
-    MatrTranslate(0, 0, 100 * Ani->JZ));
+    MatrTranslate(0, 0, 100 * Ani->JZ));*/
+  AO5_RndMatrWorld = 
+    MatrMulMatr(MatrMulMatr(MatrScale(1, 1, 1), MatrRotateY(Uni->RY)), MatrTranslate(Uni->X, Uni->Y, Uni->Z));
   //AO5_RndMatrProj = MatrFrustum(AO5_RndWp / 2, AO5_RndWp / 2, AO5_RndHp / 2, AO5_RndHp / 2, AO5_RndProjDist, 800);
   AO5_RndGObjDraw(&Uni->Model);
 } /* End of 'AO5_AnimUnitRender' function */
